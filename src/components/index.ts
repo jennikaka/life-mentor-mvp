d46d22326d9c421296cd3cfc7431f554ca598731
Jenny Hui <jiawen.hui@outlook.com>
2025-06-21 18:20:56 -0700

    feat: add component index files for cleaner imports
    - Create index files for easier component imports
    - Improve developer experience with cleaner import statements
    - Better code organization and maintainability

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { AnalysisResult } from './AnalysisResult';
+export { ReflectionForm } from './ReflectionForm';
+export { Sidebar } from './Sidebar';
+export { Themes } from './Themes';
+export { Timeline } from './Timeline';
+export * from './ui'; 
